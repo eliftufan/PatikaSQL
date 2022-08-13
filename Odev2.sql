ea72f9c80f8a812ff03d5e47298dab14bdccd438
Elif Tufan <61326756+eliftufan@users.noreply.github.com>
2022-08-13 11:42:06 +0300

    Add files via upload

---
+++ Odev2.sql
@@ -0,0 +1,3 @@
+1) select * from film where replacement_cost between 12.99 and 16.99;
+2) select first_name, last_name from actor where first_name IN ('Penelope','Nick','Ed');
+3) select * from film where rental_rate IN (0.99,2.99,4.99) AND replacement_cost IN (12.99,15.99,28.99);
