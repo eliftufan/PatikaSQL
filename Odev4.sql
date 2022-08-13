e09894a653d1f684374d4295cecd753639a59421
Elif Tufan <61326756+eliftufan@users.noreply.github.com>
2022-08-13 12:42:25 +0300

    Add files via upload

---
+++ Odev4.sql
@@ -0,0 +1,5 @@
+1) select distinct replacement_cost from film;
+2) select count(distinct replacement_cost) from film;
+3) select title,rating from film where title LIKE 'T%' AND rating='G';
+4) select count(*) from country where country LIKE '_____';
+5) select count(*) from city where city LIKE '%R' OR city LIKE '%r';
