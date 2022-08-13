e4ce5f0100c483ea52fe58b50d7dc273712d7a7c
Elif Tufan <61326756+eliftufan@users.noreply.github.com>
2022-08-13 12:13:04 +0300

    Add files via upload

---
+++ Odev3.sql
@@ -0,0 +1,3 @@
+1) select *from country where country LIKE 'A%' AND country LIKE '%a';
+2) select *from country where country LIKE '_____n%';
+4) select * from film where (title LIKE 'C%') AND (length>90) AND (rental_rate=2.99);
